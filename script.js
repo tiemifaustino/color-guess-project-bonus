92c0eff1f1ae0f1a61805195e3a7991095489b04
Tiemi Faustino <hayashipharma@gmail.com>
2022-02-11 18:20:27 -0300

    Requisito 4 ok - alterado css

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const colorContainer = document.querySelector('#color-container');
 
+// Consultei o PR da colega Tamiris Shigaki para resolver o requisito 3 - Turma 19 - A link: https://github.com/tryber/sd-019-a-project-color-guess/pull/23/files
 const addColor = () => {
   for (let index = 0; index < 6; index += 1) {
     const divColor = document.createElement('div');
     divColor.className = 'ball';
+    divColor.style.backgroundColor = `#${(Math.floor(Math.random() * 1000000))}`;
     colorContainer.appendChild(divColor);
   }
 };
 
-addColor();
+window.onload = () => {
+  addColor();
+};
